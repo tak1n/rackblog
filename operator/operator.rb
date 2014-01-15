9c012a18826a08be1036805f5e7a3b56accb08d1
Benny1992 <klotz.benjamin@yahoo.de>
2014-01-15 17:52:42 +0100

    update

--- operator/operator.rb
+++ operator/operator.rb
@@ -1,3 +1,6 @@
+require 'faraday'
+require 'json'
+
 module UserOperator
   def show_nav(documents, html)
     links = []
@@ -11,5 +14,25 @@ module UserOperator
 
     return nav
     #html["{% show_nav %}"] = nav
+  end
+
+  def show_github_activity(documents, html)
+    conn = Faraday.new(:url => 'http://osrc.dfm.io/') do |c|
+      c.use Faraday::Request::UrlEncoded  # encode request params as "www-form-urlencoded"
+      c.use Faraday::Response::Logger     # log request & response to STDOUT
+      c.use Faraday::Adapter::NetHttp     # perform requests with Net::HTTP
+    end
+
+    response = conn.get '/benny1992.json'     # GET http://sushi.com/nigiri/sake.json
+    json = response.body
+
+    datahash = JSON.parse(json)
+
+    string = "#{datahash["name"]} has following language activities: <br />"
+    datahash["usage"]["languages"].each.with_index(1) do |languagehash, index = 1|
+      string << "#{index}: #{languagehash["language"]} - #{languagehash["count"]} commits<br />"
+    end
+
+    return string
   end
 end
